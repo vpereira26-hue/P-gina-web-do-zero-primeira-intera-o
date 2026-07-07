90df64e483c31852b8f510b36c51d335baa7da39
vpereira26-hue <v.pereira26@escola.pr.gov.br>
2026-07-07 10:57:08 -0300

    ..

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const botao = document.getElementById("modo");
 
+botao.addEventListener("click", () => {
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+botao.innerHTML = "☀️ Modo Claro";
+}else{
+botao.innerHTML = "🌙 Modo Escuro";
+}
+
+});
+
+const botoes = document.querySelectorAll(".card button");
+
+botoes.forEach((botao) => {
+
+botao.addEventListener("click", () => {
+
+alert("Em breve você poderá ler este artigo completo!");
+
+});
+
+});
